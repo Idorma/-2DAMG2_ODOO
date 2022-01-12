8f6475c2e178645517103b1e6427873808f0e680
alaincosgaya <alsucar02@gmail.com>
2022-01-12 13:52:58 +0100

    Contrato funcional

--- models/granjero.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class GranjeroEntity(models.Model):
-    _name = 'lauserri.granjero'
-    _inherit = 'res.users'
-    granjas = fields.One2many('lauserri.granja', 'granjero', string="Granjas de granjero")
