2b78a20bf20d955f4751625b79c70b003066ddd9
alaincosgaya <alsucar02@gmail.com>
2022-01-12 13:56:54 +0100

    Trabajador funcional

---
+++ models/trabajador.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Trabajador(models.Model):
+    _name = 'lauserri.trabajador'
+    _inherit = 'res.users'
+    contratos = fields.One2many('lauserri.contrato','trabajador',string='Contratos del trabajador')
+    zonas = fields.Many2many('lauserri.zona', string='Zonas del trabajador')
+    
