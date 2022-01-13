bd52dcd312c11741d5049685217d089ad09d9e73
Idorma <69322475+Idorma@users.noreply.github.com>
2022-01-13 10:30:08 +0100

    Modulo desplegado con todas las clases

---
+++ models/granjero.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class GranjeroEntity(models.Model):
+    _name = 'lauserri.granjero'
+    _inherit = 'res.users'
+    granjas = fields.One2many('lauserri.granja', 'granjero', string="Granjas de granjero")
