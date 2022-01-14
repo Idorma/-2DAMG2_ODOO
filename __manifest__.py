5b3b10164347ad123e309144a93096335f04af75
alaincosgaya <alsucar02@gmail.com>
2022-01-14 12:17:53 +0100

    Contrato formulario

--- __manifest__.py
+++ __manifest__.py
@@ -26,6 +26,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
+        'views/contratos.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
