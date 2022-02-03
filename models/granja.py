3a37ef2b92cac284c5803109e78c9fa171ecb7a6
Idorma <69322475+Idorma@users.noreply.github.com>
2022-02-03 22:10:44 +0100

    Arreglo de codigo, validaciones y search view

--- models/granja.py
+++ models/granja.py
@@ -6,12 +6,12 @@ from odoo import fields
 from odoo import models
 
 class GranjaEntity(models.Model):
-      _name = 'lauserri.granja'
-      nombreGranja = fields.Char(string="Nombre de la granja")
-      fechaCreacion = fields.Date(string="Fecha de creacion de la granja")
-      granjero = fields.Many2one('lauserri.granjero', string="Granjero de la granja")
-      zonas = fields.One2many('lauserri.zona', 'granja', string="Zonas de la granja")
-      contratos = fields.One2many('lauserri.contrato', 'granja', string="Contratos de la granja")
+    _name = 'lauserri.granja'
+    nombreGranja = fields.Char(string="Nombre de la granja")
+    fechaCreacion = fields.Date(string="Fecha de creacion de la granja")
+    granjero = fields.Many2one('lauserri.granjero', string="Granjero de la granja")
+    zonas = fields.One2many('lauserri.zona', 'granja', string="Zonas de la granja")
+    contratos = fields.One2many('lauserri.contrato', 'granja', string="Contratos de la granja")
       
     @api.constrains('fechaCreacion')
     def _validar_fecha_actual(self):
